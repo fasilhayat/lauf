bafaf98cc81e72112959dc689191a8d30613ebd5
Fasil Malik Hayat <fasil.hayat@gmail.com>
2026-02-27 23:19:49 +0100

    Added resilience project

---
+++ resilience/ResilienceActor.cs
@@ -0,0 +1,9 @@
+﻿namespace MyCompany.Yggdrasil.HealthCheck;
+
+/// <summary>
+/// Provides extension methods for configuring and exposing health checks in an ASP.NET Core application.
+/// </summary>
+public static class ResilienceActor
+{
+   
+}
